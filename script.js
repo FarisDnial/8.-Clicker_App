b52af8adfc90ca52674e514649daab05103bbd62
FarisDnial <faris.danial.fauzi@gmail.com>
2024-01-31 16:30:58 +0000

    Intial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const counter = document.getElementById("counter");
+let count = 0;
+
+function increment(){
+  count++;
+  counter.textContent = count;
+}
+
+function decrement(){
+  count--;
+  counter.textContent = count;
+}
+
+//function multiplication(){
+ // count = count * 2;
+ // counter.textContent = count;
+//}
+
+//function division(){
+ // count = count / 2;
+ // counter.textContent = count;
+//}
